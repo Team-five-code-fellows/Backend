f0e6f879a1637408325e5fb1b6384345e29e1090
Dana Huffman <dana.huffman@yahoo.com>
2022-08-15 15:33:12 -0700

    Initialization

---
+++ app/game.py
@@ -0,0 +1,27 @@
+# print welcome message
+# print rules
+# ask to start or quit game
+
+# if quit fire quit sequence, print info about creators
+
+# if play prompt for game difficulty
+# Easy - set turn counter to 50(?)
+# Medium - set turn counter to 10(?)
+# Hard - set turn counter to 5
+
+# prompt user for mode
+# if random set the start page and target page using wikipedia random article functionality
+# if user input use input in wikipedia search query and grab first link as start or end point
+
+# loop while turn counter > 0 and current page isn't the target page
+# add current page to page path list
+# Scrape current page for all links
+# present page titles from links to user
+# display remaining turns
+# prompt user to input number corresponding to link or quit
+# navigate to new page
+# reduce turn counter by 1
+
+# if current page is the target present with win screen (show path list)
+# if current page is not the target present with loss screen
+# ask if the user would like to play again or quit
---
+++ app/scraper.py
@@ -0,0 +1,4 @@
+# find all links on the page
+# check that links are internal to wikipedia
+# remove duplicate links
+# format links into numbered list of page titles
