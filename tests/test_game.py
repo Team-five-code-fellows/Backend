2c329440e7e7be0c55843808e5b2718807e14c00
Dana Huffman <dana.huffman@yahoo.com>
2022-08-18 18:03:07 -0700

    Continued testing work

--- tests/test_game.py
+++ tests/test_game.py
@@ -20,22 +20,22 @@ def test_manual_same_path_win():
             expected += line
     assert actual == expected
 
-@pytest.mark.skip
-def test_manual_different_path_win():
-    with open("tests/manual_different_path_win.txt", 'r') as f:
-        responses = ['p', 'm', 'm', 'tea', 'coffee', ' ', '503', 'q']
-        actual = mocker(responses)
-        expected = ''
-        for line in f.readlines():
-            expected += line
-    assert actual == expected
-
-@pytest.mark.skip
-def test_manual_loss():
-    with open("tests/open_quit.txt", 'r') as f:
-        responses = ['q']
-        actual = mocker(responses)
-        expected = ''
-        for line in f.readlines():
-            expected += line
-    assert actual == expected
+# @pytest.mark.skip
+# def test_manual_different_path_win():
+#     with open("tests/manual_different_path_win.txt", 'r') as f:
+#         responses = ['p', 'm', 'm', 'tea', 'coffee', ' ', '503', 'q']
+#         actual = mocker(responses)
+#         expected = ''
+#         for line in f.readlines():
+#             expected += line
+#     assert actual == expected
+#
+# @pytest.mark.skip
+# def test_manual_loss():
+#     with open("tests/open_quit.txt", 'r') as f:
+#         responses = ['q']
+#         actual = mocker(responses)
+#         expected = ''
+#         for line in f.readlines():
+#             expected += line
+#     assert actual == expected
