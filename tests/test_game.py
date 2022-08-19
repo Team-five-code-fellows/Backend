e350b0a720b371809bf71ed26a82f42d99f26ba7
Vinny <vinnyshipley@gmail.com>
2022-08-19 13:18:39 -0700

    4 tests working

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,12 @@
 import pytest
 from app.game import mocker
 
+def get_local_page(name):
+    with open(f"tests/{name}.html") as f:
+        return f.read()
+
+
+
 def test_quit():
     with open("tests/open_quit.txt", 'r') as f:
         responses = ['q']
@@ -10,7 +16,7 @@ def test_quit():
             expected += line
     assert actual == expected
 
-# @pytest.mark.skip
+#@pytest.mark.skip
 def test_manual_same_path_win():
     with open("tests/manual_same_path_win.txt", 'r') as f:
         responses = ['p', 'h', 'm', 'chocolate', 'chocolate', 'q']
@@ -20,22 +26,22 @@ def test_manual_same_path_win():
             expected += line
     assert actual == expected
 
-# @pytest.mark.skip
-# def test_manual_different_path_win():
-#     with open("tests/manual_different_path_win.txt", 'r') as f:
-#         responses = ['p', 'm', 'm', 'tea', 'coffee', ' ', '503', 'q']
-#         actual = mocker(responses)
-#         expected = ''
-#         for line in f.readlines():
-#             expected += line
-#     assert actual == expected
-#
-# @pytest.mark.skip
-# def test_manual_loss():
-#     with open("tests/open_quit.txt", 'r') as f:
-#         responses = ['q']
-#         actual = mocker(responses)
-#         expected = ''
-#         for line in f.readlines():
-#             expected += line
-#     assert actual == expected
+#@pytest.mark.skip
+def test_manual_different_path_win():
+    with open("tests/manual_different_path_win.txt", 'r') as f:
+        responses = ['p', 'm', 'm', 'tea', 'coffee', '', '503', 'q']
+        actual = mocker(responses)
+        expected = ""
+        for line in f.readlines():
+            expected += line
+    assert actual == expected
+
+#@pytest.mark.skip
+def test_manual_loss():
+    with open("tests/manual_loss.txt", 'r') as f:
+        responses = ['p', 'h', 'm', 'road', 'buildings', '', '2', '', '5', '', '6', '', '5', '', '5', 'n', 'q']
+        actual = mocker(responses)
+        expected = ''
+        for line in f.readlines():
+            expected += line
+    assert actual == expected
